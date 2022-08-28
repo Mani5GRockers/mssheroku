6f96f71ecc47249886594f4da144fbfdfa56d7c7
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2022-08-28 13:54:28 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,8 +47,9 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Owner", "https://www.github.com/anasty17")
+    buttons.buildbutton("Ⓜ️ ᴍɪʀʀᴏʀ ɢʀᴏᴜᴘ Ⓜ️", "https://t.me/awsmirrorzone")
+    buttons.buildbutton("🦸 Chat ᴍɪʀʀᴏʀ ɢʀᴏᴜᴘ 🦸‍♂️", "@aws_public_chat")
+    buttons.buildbutton("🌐 Website 🌐", "https://hifimass.in")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
