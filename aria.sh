428a308ffb87d438cd3cc478be8fdfdc21fca3bf
Mani5GRockers <66015149+Mani5GRockers@users.noreply.github.com>
2022-09-23 23:18:40 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -9,5 +9,5 @@ aria2c --allow-overwrite=true --auto-file-renaming=true --bt-enable-lpd=true --b
        --daemon=true --disk-cache=40M --enable-rpc=true --follow-torrent=mem --force-save=true --http-accept-gzip=true \
        --max-connection-per-server=10 --max-concurrent-downloads=10 --max-file-not-found=0 --max-tries=20 \
        --min-split-size=10M --netrc-path=/usr/src/app/.netrc --optimize-concurrent-downloads=true \
-       --peer-id-prefix=-qB4440- --peer-agent=qBittorrent/4.4.4 --quiet=true --reuse-uri=true \
+       --peer-id-prefix=-qB4445- --peer-agent=qBittorrent/4.4.5 --quiet=true --reuse-uri=true \
        --rpc-max-request-size=1024M --seed-ratio=0 --split=10 --summary-interval=0 --user-agent=Wget/1.12
